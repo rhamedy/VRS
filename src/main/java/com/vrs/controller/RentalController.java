7cd0684cad8d92cad2bacdc1da51b6cfd8367e2d
dev-group <rafiullahrs@gmail.com>
2013-02-21 20:20:14 +0000

    configured logging using logback framework - raf

--- src/main/java/com/vrs/controller/RentalController.java
+++ src/main/java/com/vrs/controller/RentalController.java
@@ -1,5 +1,7 @@
 package com.vrs.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,11 +9,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class RentalController {
 	
+	private static Logger logger = LoggerFactory.getLogger(RentalController.class); 
+	
 	@RequestMapping(value = "/home")
 	public ModelAndView homepage() { 
+		logger.info("inside controller {info}"); 
 		ModelAndView mav = new ModelAndView(); 
 		
 		mav.setViewName("index");
+		logger.debug("at the end of the controller{degub}"); 
 		
 		return mav; 
 	}
