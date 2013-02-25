02ac7adf44bbfe533a0fdb38c61dea1165c475e4
dev-group <rafiullahrs@gmail.com>
2013-02-25 02:19:58 +0000

    Role model class to hold different user roels - raf

---
+++ src/main/java/com/vrs/model/Role.java
@@ -0,0 +1,27 @@
+package com.vrs.model;
+
+public class Role {
+
+	/**
+	 * Role class is used as a placeholder for available roles
+	 * 
+	 * @Author Rafiullah Hamedy
+	 * @Date   25-02-2013
+	 */
+	
+	private int roleId; 
+	private String roleName; 
+	
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+}
