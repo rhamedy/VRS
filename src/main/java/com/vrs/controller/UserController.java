5e1c583d67bffb467eb98a2afdaf997fc3cdbead
dev-group <rafiullahrs@gmail.com>
2013-03-15 13:00:54 +0000

    method to list countries, cities of a country, filter to allow js files publicly - raf

--- src/main/java/com/vrs/controller/UserController.java
+++ src/main/java/com/vrs/controller/UserController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.vrs.services.RentalServices;
@@ -15,34 +17,43 @@ import com.vrs.services.UserServices;
 
 @Controller
 public class UserController {
-	
-	private static Logger logger = LoggerFactory.getLogger(UserController.class); 
-	
-	@Autowired 
-	private UserServices userServices; 
-	
-	@Autowired 
-	private RentalServices rentalServices; 
-	
-	@RequestMapping(value="/home/public")
-	public ModelAndView showHomepage() { 
-		logger.info("entry showHomepage()"); 
-		
-		ModelAndView mav = new ModelAndView(); 
-		mav.setViewName("public"); 
-		
-		List<Map<Integer, String>> countries = rentalServices.getCountries(); 
-		
-		logger.info("countries size{}", countries.size()); 
-		
-		for(Map<Integer, String> m: countries) { 
-			for(Map.Entry<Integer, String> entry: m.entrySet()) { 
-				logger.info("country.key: {}, country.name: {}", entry.getKey(), entry.getValue()); 
-			}
-		}
-		
-		mav.addObject("countries", countries); 
-		
-		return mav; 
+
+	private static Logger logger = LoggerFactory
+			.getLogger(UserController.class);
+
+	@Autowired
+	private UserServices userServices;
+
+	@Autowired
+	private RentalServices rentalServices;
+
+	@RequestMapping(value = "/home/public")
+	public ModelAndView showHomepage() {
+		logger.info("entry showHomepage()");
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("public");
+
+		List<Map<Integer, String>> countries = rentalServices.getCountries();
+
+		logger.info("countries size{}", countries.size());
+
+		// for(Map<Integer, String> m: countries) {
+		// for(Map.Entry<Integer, String> entry: m.entrySet()) {
+		// logger.info("country.key: {}, country.name: {}", entry.getKey(),
+		// entry.getValue());
+		// }
+		// }
+
+		mav.addObject("countries", countries);
+
+		return mav;
+	}
+
+	@RequestMapping(value = "/home/public/cities")
+	public @ResponseBody
+	List<Map<Integer, String>> getCities(@RequestParam String countryId) {
+		int parsedCountryId = Integer.parseInt(countryId);
+		return rentalServices.getCities(parsedCountryId); 
 	}
 }
