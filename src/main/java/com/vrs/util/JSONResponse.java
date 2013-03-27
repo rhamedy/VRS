9199143269dda23ac895505d1bb0877cb86b7d03
dev-group <rafiullahrs@gmail.com>
2013-03-27 02:29:46 +0000

    initialized the variables to initial value to avoid null in jquery- raf

--- src/main/java/com/vrs/util/JSONResponse.java
+++ src/main/java/com/vrs/util/JSONResponse.java
@@ -1,13 +1,30 @@
 package com.vrs.util;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class models a response with a status message indicating 
+ * success or failure, a message and a list of errors. An object 
+ * of this class will be used through out the system to inform the 
+ * browser of activities status. 
+ * 
+ * @author Rafiullah Hamedy
+ * @Date 26-03-2013
+ * 
+ */
 
 public class JSONResponse {
 	
 	private String status; 
 	private String message; 
-	private List<KeyValuePair<String, String>> errors;
+	private List<KeyValuePair<String, String>> errors; 
+	
+	public JSONResponse() { 
+		this.errors = new ArrayList<KeyValuePair<String, String>>();
+		this.message = ""; 
+		this.status = "failure";
+	}
 	
 	public String getStatus() {
 		return status;
