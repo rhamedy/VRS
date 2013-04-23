b7b5b05274d264552223f71e4b76ea568f12d1c0
dev-group <rafiullahrs@gmail.com>
2013-04-23 02:35:09 +0100

    added booking object to encapsulate the booking fields #4 - raf

---
+++ src/main/java/com/vrs/model/Booking.java
@@ -0,0 +1,87 @@
+package com.vrs.model;
+
+import java.sql.Date;
+
+/**
+ * Booking clas with encapsulate all the fields that is required for booking a vehicle.
+ * The customer contact, booking start date and end date, insurance details, price and 
+ * other useful information will all get bundled into Booking object to make it easy 
+ * passing around and increase performance. 
+ * 
+ *  @author Rafiullah Hamedy
+ *  @Date	22-04-2013
+ */
+
+public class Booking {
+	private String id; 
+	private String vehicleVin; 
+	private String username; 
+	private Date startDate; 
+	private Date endDate; 
+	private boolean driver = false; 
+	private boolean insurance = false; 
+	private int extensionCount; 
+	private double hireCost; 
+	private double damageCost;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getVehicleVin() {
+		return vehicleVin;
+	}
+	public void setVehicleVin(String vehicleVin) {
+		this.vehicleVin = vehicleVin;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	public boolean isDriver() {
+		return driver;
+	}
+	public void setDriver(boolean driver) {
+		this.driver = driver;
+	}
+	public boolean isInsurance() {
+		return insurance;
+	}
+	public void setInsurance(boolean insurance) {
+		this.insurance = insurance;
+	}
+	public int getExtensionCount() {
+		return extensionCount;
+	}
+	public void setExtensionCount(int extensionCount) {
+		this.extensionCount = extensionCount;
+	}
+	public double getHireCost() {
+		return hireCost;
+	}
+	public void setHireCost(double hireCost) {
+		this.hireCost = hireCost;
+	}
+	public double getDamageCost() {
+		return damageCost;
+	}
+	public void setDamageCost(double damageCost) {
+		this.damageCost = damageCost;
+	} 
+}
